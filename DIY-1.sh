3d5fcac37958b98096ada83e8e9ce8d984a29a8b
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-11 11:16:49 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -17,5 +17,5 @@ git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adg
 # sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 6-添加 PassWall 插件
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"

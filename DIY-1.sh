2c430bb37d8c72a9d819e815434bdf7dc5ca1c6e
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-15 10:55:30 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -20,7 +20,5 @@ git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adg
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
-
-
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
